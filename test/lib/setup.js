5f6e75672dbcb558dad7ec7c710aaefcb7605476
Ingo Fischer New <ingof@GF0459MAC.local>
2016-12-14 16:32:03 +0100

    update testing-setup.js

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -34,11 +34,7 @@ function copyFileSync(source, target) {
         }
     }
 
-    try {
-        fs.writeFileSync(targetFile, fs.readFileSync(source));
-    } catch (e) {
-        console.log('ERROR copying file ' + source + ' to ' + targetFile);
-    }
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
 
 function copyFolderRecursiveSync(source, target, ignore) {
@@ -119,7 +115,8 @@ function restoreOriginalFiles() {
 
 }
 
-function checkIsAdapterInstalled(cb, counter) {
+function checkIsAdapterInstalled(cb, counter, customName) {
+    customName = customName || pkg.name.split('.').pop();
     counter = counter || 0;
     var dataDir = rootDir + 'tmp/' + appName + '-data/';
     console.log('checkIsAdapterInstalled...');
@@ -127,7 +124,7 @@ function checkIsAdapterInstalled(cb, counter) {
     try {
         var f = fs.readFileSync(dataDir + 'objects.json');
         var objects = JSON.parse(f.toString());
-        if (objects['system.adapter.' + pkg.name.split('.').pop() + '.0']) {
+        if (objects['system.adapter.' + customName + '.0']) {
             console.log('checkIsAdapterInstalled: ready!');
             setTimeout(function () {
                 if (cb) cb();
@@ -181,12 +178,17 @@ function checkIsControllerInstalled(cb, counter) {
     }
 }
 
-function installAdapter(cb) {
+function installAdapter(customName, cb) {
+    if (typeof customName === 'function') {
+        cb = customName;
+        customName = null;
+    }
+    customName = customName || pkg.name.split('.').pop();
     console.log('Install adapter...');
     var startFile = 'node_modules/' + appName + '.js-controller/' + appName + '.js';
     // make first install
     if (debug) {
-        child_process.execSync('node ' + startFile + ' add ' + pkg.name.split('.').pop() + ' --enabled false', {
+        child_process.execSync('node ' + startFile + ' add ' + customName + ' --enabled false', {
             cwd:   rootDir + 'tmp',
             stdio: [0, 1, 2]
         });
@@ -197,7 +199,7 @@ function installAdapter(cb) {
         });
     } else {
         // add controller
-        var _pid = child_process.fork(startFile, ['add', pkg.name.split('.').pop(), '--enabled', 'false'], {
+        var _pid = child_process.fork(startFile, ['add', customName, '--enabled', 'false'], {
             cwd:   rootDir + 'tmp',
             stdio: [0, 1, 2, 'ipc']
         });
@@ -667,6 +669,7 @@ if (typeof module !== undefined && module.parent) {
     module.exports.setupController  = setupController;
     module.exports.stopAdapter      = stopAdapter;
     module.exports.startAdapter     = startAdapter;
+    module.exports.installAdapter   = installAdapter;
     module.exports.appName          = appName;
     module.exports.adapterName      = adapterName;
 }
